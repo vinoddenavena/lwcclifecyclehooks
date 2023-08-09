094bafc0a2d194d571d8a0777a14fc94aff738d4
vinoddenavena <115385596+vinoddenavena@users.noreply.github.com>
2023-08-09 11:22:11 +0530

    publish

---
+++ .sfdx/typings/lwc/apex/searchDynamically.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/searchDynamically.stringtest" {
+  export default function stringtest(param: {inputData: any, objectName: any, ApiFields: any, field1: any, field2: any, field3: any}): Promise<any>;
+}
